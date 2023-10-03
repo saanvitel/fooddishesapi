504a6942cc6b205263f154edd132b30302fd16a8
Saanvi Telukoti <saanvitelukoti@Saanvis-MBP.localdomain>
2023-10-03 11:22:10 +1100

    all get methods work

--- webapi.rb
+++ webapi.rb
@@ -3,6 +3,7 @@ require 'sinatra'
 require 'json'
 require 'sqlite3'
 
+#method that simplifies process of getting data based on params in websites URL
 def make_query(query, *params)
   db = SQLite3::Database.open 'foodndishes.db'
   db.results_as_hash = true
@@ -10,9 +11,11 @@ def make_query(query, *params)
   array = []
 
   stm = db.prepare(query)
-  stm.bind_param(params)
+  params.each_with_index do |param, index|
+    stm.bind_param(index + 1, param)
+  end
 
-  result_set = stm.execute
+  result_set = stm.execute(query)
 
   while (row = result_set.next) do
       array.push(row)
@@ -31,7 +34,8 @@ get '/' do
   'This is sinatra web api response 😎'
 end
 
-get '/:ingredient' do
+#search
+get '/search/:ingredient' do
   ingredient = params[:ingredient]
   ingredient ||= ingredient.split.map(&:capitalize).join(' ')
   result = make_query("SELECT * FROM dishes AS d JOIN ingredients_in_dish USING(dishes_id) JOIN ingredients AS i USING(ingredients_id) WHERE i.name =?;", ingredient)
@@ -55,10 +59,27 @@ get '/dishesin/:cuisine' do
 end
 
 #will fetch dishes based on it being  vegetarian or vegans 
+#doesnt work with value without params for some reason -- wth
+#using normal seperate format ----
 get '/veganveg' do
-  value = 1
-  result = make_query("SELECT * FROM dishes WHERE vegetarian_or_vegan =?;", value)
-  return result.to_json
+  db = SQLite3::Database.open 'foodndishes.db'
+  db.results_as_hash = true
+
+  array = []
+  
+  result_set = db.query("SELECT * FROM dishes WHERE vegetarian_or_vegan =1;")
+
+  while (row = result_set.next) do
+      array.push(row)
+  end
+
+  return array.to_json
+
+  rescue SQLite3::Exception => e 
+  
+    puts "Exception occurred"
+    puts e
+    []
 end
 
 #will fetch dishes based on it being dishes name
@@ -69,21 +90,21 @@ get '/dish/:name' do
   return result.to_json
 end
 
-post '/disheswith/:ingredient' do
-  ingredient = JSON.parse(request.body.read)
-  ingredients[ingredient['name'].downcase.to_sym] = ingredient
-
-  url = "http://localhost:4567/users/#{ingredient['name']}"
-  response.headers['Location'] = url   
-  status 201
-end
-
-# post '' do
+# post '/disheswith/:ingredient' do
 #   ingredient = JSON.parse(request.body.read)
-#   # ingredient = params[:ingredient]
 #   ingredients[ingredient['name'].downcase.to_sym] = ingredient
 
 #   url = "http://localhost:4567/users/#{ingredient['name']}"
 #   response.headers['Location'] = url   
 #   status 201
 # end
+
+post '/veganveg' do
+  ingredient = JSON.parse(request.body.read)
+  # ingredient = params[:ingredient]
+  ingredients[ingredient['name'].downcase.to_sym] = ingredient
+
+  url = "http://localhost:4567/users/#{ingredient['name']}"
+  response.headers['Location'] = url   
+  status 201
+end
